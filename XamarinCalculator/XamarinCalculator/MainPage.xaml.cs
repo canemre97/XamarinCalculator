829de261c3b60088fecd6f71a74a9ceb0f5408b3
Can EMRE <30720714+canemre97@users.noreply.github.com>
2023-04-25 15:24:11 +0300

    Add project files.

---
+++ XamarinCalculator/XamarinCalculator/MainPage.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace XamarinCalculator
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnHesapla_OnClicked(object sender, EventArgs e)
+        {
+            if (entrySayi1.Text == null || entrySayi2.Text == null)
+            {
+                DisplayAlert("Hata!", "Lütfen tüm alanları doldurunuz.", "Kapat");
+                return;
+            }
+            var sayi1 = Convert.ToDouble(entrySayi1.Text);
+            var sayi2 = Convert.ToDouble(entrySayi2.Text);
+            var btn = (Button)sender;
+            double sonuc = 0;
+            switch (btn.Text)
+            {
+                case "+":
+                    sonuc = sayi1 + sayi2;
+                    break;
+                case "-":
+                    sonuc = sayi1 - sayi2;
+                    break;
+                case "*":
+                    sonuc = sayi1 * sayi2;
+                                        break;
+                case "/":
+                    sonuc = sayi1 / sayi2;
+                    break;
+                default:
+                    lblSonuc.Text = "İşlem Başarısız";
+                    break;
+            }
+            lblSonuc.Text = sonuc.ToString();
+            lblSnc.IsVisible = true;
+            lblSonuc.IsVisible = true;
+        }
+    }
+}
